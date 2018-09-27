bcadb8de2c92e4002ee3ef3d55a639951610ea98
Dan Hintz <dhintz@live.com>
2018-09-27 23:14:23 +0000

    automatically backed up by learn

--- lib/models/musicLibraryController.rb
+++ lib/models/musicLibraryController.rb
@@ -8,8 +8,8 @@ class MusicLibraryController
   def call
     puts "Welcome to your music library!"
     puts "To list all of your songs, enter 'list songs'."
-    puts ""
-    puts ""
+    puts "To list all of the arists, enter 'list '."
+    puts "To list all of the , enter 'list '."
     puts ""
     puts ""
     puts ""
