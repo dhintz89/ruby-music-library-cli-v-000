71df0740c59dd4605d0c9fec9dbe84aac62f52f8
Dan Hintz <dhintz@live.com>
2018-09-27 23:25:22 +0000

    automatically backed up by learn

--- lib/models/musicLibraryController.rb
+++ lib/models/musicLibraryController.rb
@@ -10,7 +10,7 @@ class MusicLibraryController
     until user_input == "exit" || user_input == "Exit"
       puts "Welcome to your music library!"
       puts "To list all of your songs, enter 'list songs'."
-      puts "To list all of the arists in your library, enter 'list artists'."
+      puts "To list all of the artists in your library, enter 'list artists'."
       puts "To list all of the genres, enter 'list genres'."
       puts "To list all of the songs by a particular artist, enter   'list artist'."
       puts "To list all of the songs of a particular genre, enter   'list genre'."
