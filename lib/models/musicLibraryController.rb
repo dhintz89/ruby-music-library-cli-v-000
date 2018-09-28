6fc77a38f7d8c2962d833c8efd626cc7e4452449
Dan Hintz <dhintz@live.com>
2018-09-28 00:48:25 +0000

    automatically backed up by learn

--- lib/models/musicLibraryController.rb
+++ lib/models/musicLibraryController.rb
@@ -54,7 +54,6 @@ class MusicLibraryController
     puts "Which song number would you like to play?"
     list_songs
     user_index = gets.strip
-    selected_song = list_songs[user_index-1]
     puts "Playing #{selected_song.name} by #{selected_song.artist.name}"
   end
 
