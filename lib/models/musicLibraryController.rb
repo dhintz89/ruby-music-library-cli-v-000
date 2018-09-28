3a2b7f8db41b0089b4240ed276f9fbe988c5794d
Dan Hintz <dhintz@live.com>
2018-09-28 02:01:15 +0000

    automatically backed up by learn

--- lib/models/musicLibraryController.rb
+++ lib/models/musicLibraryController.rb
@@ -52,7 +52,6 @@ class MusicLibraryController
 
   def play_song
     list = Song.all.sort_by {|song| song.name}
-    begin
       puts "Which song number would you like to play?"
       index = gets.strip.to_i - 1
     end until index >= 0 && index <= list.length
