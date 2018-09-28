006e097b9a61544f8ef35d7e3f3c6f904d5889a6
Dan Hintz <dhintz@live.com>
2018-09-28 01:41:01 +0000

    automatically backed up by learn

--- lib/models/musicLibraryController.rb
+++ lib/models/musicLibraryController.rb
@@ -52,19 +52,10 @@ class MusicLibraryController
 
   def play_song
     puts "Which song number would you like to play?"
-
     list = Song.all.sort_by {|song| song.name}
     selection = gets.strip
     index = selection.to_i-1
     puts "Playing #{list[index].name} by #{list[index].artist.name}"
-    
-    
   end
-  #   list = Song.all.sort_by {|song| song.name}
-  #   selection = gets.strip
-  #   index = selection.to_i - 1
-  #   selected_song = list[index]
-  #   puts "Playing #{selected_song.name} by #{selected_song.artist.name}"
-  # end
-
+  
 end
